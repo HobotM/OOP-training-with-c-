b09ade76e76ef54bede387fe7ac41a7b36080c23
HobotM <mateuszhobot@gmail.com>
2023-04-29 18:21:35 +0100

    Add project files.

---
+++ Computer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_example
+{
+    abstract class Computer: IPowerOn
+    {
+
+        protected IPowerOn powerController;
+
+        protected string _name = "Unknown";
+
+
+        public bool isOn { get { return powerController.isOn; } }
+        public virtual string name
+        {
+            get
+            {
+                return _name;
+            }
+            private set
+            {
+                _name = value;
+            }
+        }
+
+        public Computer(string name)
+        {
+            this.name = name;
+            powerController = new PowerController();
+
+        }
+
+        public virtual void TogglePower()
+        {
+            powerController.TogglePower();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace OOP_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+             //var comp = new Desktop("Generic computer", Desktop.CaseType.Tower);
+
+            var comp = new Laptop("Generic laptop", 1024, 768);
+
+            Console.WriteLine("Computer Type - " + comp.GetType());
+
+            var isComputer = comp is Computer;
+            Console.WriteLine("is computer = " + isComputer);
+            Console.WriteLine("Computer name = " + comp.name);
+            //Console.WriteLine("Computer case = " + comp.caseType);
+
+            comp.TogglePower();
+            Console.WriteLine("Computer is on - " + comp.isOn);
+
+            var sleepingComp = comp as ISleep;
+
+            if (sleepingComp != null)
+            {
+                sleepingComp.ToggleSleep();
+                Console.WriteLine("Computer is sleeping - " + sleepingComp.isSleeping);
+            }
+
+            comp.TogglePower();
+            Console.WriteLine("Computer is on - " + comp.isOn);
+
+            Console.Read();
+
+        }
+    }
+}
